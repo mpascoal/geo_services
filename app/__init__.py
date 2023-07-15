8b6de4dbb6d5b1289cc0f8ce98f0ba2d28ab628d
Marcio Paschoal <mpascoal@gmail.com>
2023-07-15 13:48:23 +0000

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from .routes import *
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/models/user.py
@@ -0,0 +1,4 @@
+import datetime 
+from app import db, ma
+
+class Users()
